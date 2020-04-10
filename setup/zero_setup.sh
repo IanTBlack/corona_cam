73ccef0957f9ea871d73d12ab3cff618ab684613
Ian Black <blackia@oregonstate.edu>
2020-04-10 11:46:43 -0700

    Updated script to shutdown pi instead of reboot.

--- setup/zero_setup.sh
+++ setup/zero_setup.sh
@@ -6,6 +6,7 @@ wget http://www.uugear.com/repo/Zero2GoOmini/installZero2Go.sh
 sudo sh installZero2Go.sh
 echo "Zero2Go Omini software has been installed."
 
-echo "Rebooting in 10 seconds..."
+echo "Shutting down in 10 seconds..."
+echo "After shutdown, you can install the Zero2Go Omini and power up the Pi again."
 sleep 10s
-sudo reboot
+sudo shutdown -h now
